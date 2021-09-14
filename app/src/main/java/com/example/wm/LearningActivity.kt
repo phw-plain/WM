7fe94f5310f859dc43e1cecc7fe865ba0a580fb5
phw-plain <s2028@e-mirim.hs.kr>
2021-09-14 20:05:26 +0900

    learn commit 1

---
+++ app/src/main/java/com/example/wm/LearningActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LearningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning)
+
+
+    }
+}
--- app/src/main/java/com/example/wm/MainActivity.kt
+++ app/src/main/java/com/example/wm/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.wm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.text.HtmlCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val LearnButton = findViewById<ImageButton>(R.id.learn)
+        LearnButton.setOnClickListener {
+            val intent = Intent(this, LearningActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
